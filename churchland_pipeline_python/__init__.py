7120d23ec5b17c5b166507b82d1b7ea4429deef6
Najja Marshall <najja.marshall@gmail.com>
2020-09-24 20:17:26 -0400

    Removed common.py file from __all__

--- churchland_pipeline_python/__init__.py
+++ churchland_pipeline_python/__init__.py
@@ -4,7 +4,7 @@ import datajoint as dj
 
 # update list of all modules
 modules = glob.glob(join(dirname(__file__), "*.py"))
-__all__ = [basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
+__all__ = [basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py') and not f.endswith('common.py')]
 
 dj.config['stores'] = {
     'churchlandlocker': {

5900818471272206721184e49f28b5ebc579abfa
Najja Marshall <najja.marshall@gmail.com>
2020-11-11 00:22:22 -0500

    Updated requirements and __init__
    Modified __init__ to only update database prefix for sandbox installation

--- churchland_pipeline_python/__init__.py
+++ churchland_pipeline_python/__init__.py
@@ -6,8 +6,9 @@ from dotenv import load_dotenv, find_dotenv
 # load env file
 load_dotenv(join(dirname(__file__), '..', '.env')) 
 
-# write mode to database prefix
-dj.config['database.prefix'] = (os.getenv("MODE") + '_' if os.getenv("MODE") is not None else '')
+# write mode to database prefix (only for sandbox installation)
+dj.config['database.prefix'] = (os.getenv("MODE") + '_' \
+    if not dj.config['database.host'].startswith('datajoint.u19motor.zi.columbia.edu') and os.getenv("MODE") is not None else '')
 
 # update list of all modules
 modules = glob.glob(join(dirname(__file__), "*.py"))
